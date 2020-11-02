5fb8fcfde43c410d0746edae6544efa46c147ad5
chinnamlakshmibhavani <chinnamlakshmi1161999@gmail.com>
2020-11-02 10:35:36 +0530

    Refactoring the code to write a class method

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,34 +1,51 @@
-public class EmpWageComputation 
+class EmpWageComputation 
 {
   public static final int IS_PART_TIME = 1;
   public static final int IS_FULL_TIME = 2;
   public static final int empRatePerHr = 20;
   public static final int NUM_OF_WORKING_DAYS=20;
-  public static final int MAX_HRS_IN_MONTH=100; 
-  public static void main(String args[])
+  public static final int MAX_HOURS_IN_MONTH=100; 
+  int empHrs = 0;
+  int empWage=0;
+  int totalEmpWage=0;
+  int totalWorkingHrs = 0;
+  int totalWorkingDays=0;
+
+  public void wageComputation()
   {
-      int empHrs = 0;
-      int totalEmpHrs = 0;
-      int totalWorkingDays=0;
-      while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
-      {
+     empWage=empHrs*empRatePerHr;
+     totalEmpWage+=empWage;
+     totalWorkingHrs+=empHrs;
+     System.out.println("Employee's day " + totalWorkingDays + " Wage is: " +empWage);
+  }
+  public void computation()
+  {
+     while(totalWorkingDays<NUM_OF_WORKING_DAYS && totalWorkingHrs<MAX_HOURS_IN_MONTH)
+     {
         totalWorkingDays++;
         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
         switch (empCheck) 
            {
 		case IS_FULL_TIME : 
-		       empHrs = 8;
-			break;
+                      System.out.println("Employee is full time working");
+		      empHrs = 8;
+		      break;
 		case IS_PART_TIME :
-			empHrs = 4;
-			break;
+                      System.out.println("Employee is working part time");
+		      empHrs = 4;
+		      break;
 		default :
-			empHrs = 0;
+                      System.out.println("Employee is absent");
+		      empHrs = 0;
 	   }
-     totalEmpHrs+=empHrs;
-     System.out.println("Day: " + totalWorkingDays+ "Emp Hr:" +empHrs);
-     }
-     int totalEmpWage=totalEmpHrs*empRatePerHr;
-     System.out.println("Total Emp Wage:" +totalEmpWage);
+        wageComputation();
+      }   
+     System.out.println("Total working hours in month:"+totalWorkingHrs);
+     System.out.println("Total monthly wages is:"+totalEmpWage);
   }
-}
+     
+  public static void main(String args[]){
+        EmpWageComputation obj=new EmpWageComputation();
+        obj.computation();
+    }
+ }
